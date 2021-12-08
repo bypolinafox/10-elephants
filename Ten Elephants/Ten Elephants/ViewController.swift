16ca2100a8fba45b165986d3a978dfbdb10deeb0
Kirill Denisov <kirill.denisov700@gmail.com>
2021-12-08 20:26:20 +0300

    Create Xcode project, set portrait orientation, deployment target to 13.0 IOS iPhone, add app icon

---
+++ Ten Elephants/Ten Elephants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ten Elephants
+//
+//  Created by Kirill Denisov on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
