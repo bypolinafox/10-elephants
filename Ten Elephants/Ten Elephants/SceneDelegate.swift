9a027bb02c500d9f7e1f7d1a9c98517eb9a38fda
Kirill Denisov <kirill.denisov700@gmail.com>
2021-12-09 16:16:01 +0300

    change scene delegate
    change delegate of scene

--- Ten Elephants/Ten Elephants/SceneDelegate.swift
+++ Ten Elephants/Ten Elephants/SceneDelegate.swift
@@ -13,6 +13,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, DOES NOT USE SB
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
         guard let winScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: winScene)
