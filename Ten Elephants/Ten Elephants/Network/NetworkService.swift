fc6e1e2257b7d3f9d364de95383b14f2741ec13c
bypolinafox <polina20012012@yandex.ru>
2021-12-14 16:08:16 +0300

    close #46: exponential backoff

--- Ten Elephants/Ten Elephants/Network/NetworkService.swift
+++ Ten Elephants/Ten Elephants/Network/NetworkService.swift
@@ -8,13 +8,42 @@
 import Foundation
 
 final class NetworkService: NetworkServiceProtocol {
+    private lazy var delayCounter = ExponentialBackoffDelayCalculator()
     
     func getMealDetails(id: Int, completion: @escaping mealsCompletion) {
-        request(type: .detailsById(id: id), completion: completion)
+        request(type: .detailsById(id: id)) { [weak self] (result: Result<Meals, NetworkFetchingError>) in
+            completion(result)
+            guard let self = self else { return }
+            if case .failure = result {
+                let newDelay = self.delayCounter.countDelay()
+                print("Retry after \(newDelay)")
+                DispatchQueue.main.asyncAfter(
+                    deadline: .now() + newDelay, execute: { [weak self] in
+                        self?.getMealDetails(id: id, completion: completion)
+                    }
+                )
+            } else {
+                self.delayCounter.resetDelay()
+            }
+        }
     }
     
     func getRandomMeals(completion: @escaping mealsCompletion) {
-        request(type: .randomMeals, completion: completion)
+        request(type: .randomMeals) { [weak self] (result: Result<Meals, NetworkFetchingError>) in
+            completion(result)
+            guard let self = self else { return }
+            if case .failure = result {
+                let newDelay = self.delayCounter.countDelay()
+                print("Retry after \(newDelay)")
+                DispatchQueue.main.asyncAfter(
+                    deadline: .now() + newDelay, execute: { [weak self] in
+                        self?.getRandomMeals(completion: completion)
+                    }
+                )
+            } else {
+                self.delayCounter.resetDelay()
+            }
+        }
     }
 
     func getFullIngredientsList(completion: @escaping (Result<FullIngredients, NetworkFetchingError>) -> Void) {
@@ -22,21 +51,33 @@ final class NetworkService: NetworkServiceProtocol {
     }
     
     func getFilteredMealList(ingredient: String, completion: @escaping mealsCompletion) {
-        request(
-            type: .mealsByIngredient(ingredient: ingredient),
-            completion: completion
-        )
+        request(type: .mealsByIngredient(ingredient: ingredient)) { [weak self] (result: Result<Meals, NetworkFetchingError>) in
+            completion(result)
+            guard let self = self else { return }
+            if case .failure = result {
+                let newDelay = self.delayCounter.countDelay()
+                print("Retry after \(newDelay)")
+                DispatchQueue.main.asyncAfter(
+                    deadline: .now() + newDelay, execute: { [weak self] in
+                        self?.getFilteredMealList(ingredient: ingredient, completion: completion)
+                    }
+                )
+            } else {
+                self.delayCounter.resetDelay()
+            }
+        }
     }
     
 }
 
 extension NetworkService {
+    
     private func request<T: Decodable>(type: RequestType, completion: @escaping (Result<T, NetworkFetchingError>) -> Void) {
         guard let url = type.url else {
             completion(.failure(NetworkFetchingError.unableToMakeURL))
             return
         }
-        print(url)
+        
         URLSession.shared.dataTask(with: url) {
             data, _, error in
             if let error = error {
@@ -56,6 +97,6 @@ extension NetworkService {
                 completion(.failure(.parsingError))
             }
         }.resume()
+        
     }
 }
-
