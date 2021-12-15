90ba85ae701c2a216dd84212f31f96585523ab7f
bypolinafox <polina20012012@yandex.ru>
2021-12-15 20:16:41 +0300

    close #31: query via operations

--- TenElephants/TenElephants/AppDelegate.swift
+++ TenElephants/TenElephants/AppDelegate.swift
@@ -9,6 +9,7 @@ import UIKit
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
+
     func application(
         _: UIApplication,
         didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
